74b19128fbcb1e5c5df1362b55b1db4831ba7d31
Yunus Emre Kara <yemregaming1@outlook.com>
2021-01-24 00:50:34 +0300

    a

--- MyDictonary/MyDictionary.cs
+++ MyDictonary/MyDictionary.cs
@@ -7,6 +7,7 @@ namespace MyDictonary
     class MyDictionary<T>
     {
         T[] arrays;
+
         public MyDictionary()
         {
             arrays = new T[0];
@@ -20,6 +21,29 @@ namespace MyDictonary
                 arrays[i] = temparray[i];
             }
             arrays[arrays.Length - 1] = array;
+        }
+    }
+    class MyDictionary<T,Z>
+    {
+        T[] arraysT;
+        Z[] arraysZ;
+        public MyDictionary()
+        {
+            arraysT = new T[0];
+            arraysZ = new Z[0];
+        }
+        public void Add(T arrayT,Z arrayZ)
+        {
+            T[] temparrayT = arraysT;
+            Z[] temparrayZ = arraysZ;
+            arraysT = new T[arraysT.Length + 1];
+            for (int i = 0; i < temparrayT.Length; i++)
+            {
+                arraysT[i] = temparrayT[i];
+                arraysZ[i] = temparrayZ[i];
+            }
+            arraysT[arraysT.Length - 1] = arrayT;
+            arraysZ[arraysT.Length - 1] = arrayZ;
         }
     }
 }
--- MyDictonary/Program.cs
+++ MyDictonary/Program.cs
@@ -14,9 +14,25 @@ namespace MyDictonary
             meyveler.Add("şeftali");
             meyveler.Add("elma");
 
-            my
-
             Console.WriteLine(meyveler);
+
+            MyDictionary<int> sayılar = new MyDictionary<int>();
+            sayılar.Add(1);
+            sayılar.Add(100);
+            sayılar.Add(73);
+            sayılar.Add(82);
+
+            Console.WriteLine(sayılar);
+            /*
+            MyDictionary<int,Sehirler> sehirler = new MyDictionary<Sehirler>();
+            sehirler.Add(10);
+            Console.WriteLine(meyveler);
+            */
+        }
+        public class Sehirler
+        {
+            public string SehirAdi { get; set; }
+            public int Plakası { get; set; }
         }
     }
 
