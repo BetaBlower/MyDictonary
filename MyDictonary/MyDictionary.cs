1e36727196854518fb88d96eae24b3ae83e95b9b
Yunus Emre Kara <yemregaming1@outlook.com>
2021-01-23 20:56:18 +0300

    Add project files.

---
+++ MyDictonary/MyDictionary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictonary
+{
+    class MyDictionary<T>
+    {
+        T[] arrays;
+        public MyDictionary()
+        {
+            arrays = new T[0];
+        }
+        public void Add(T array)
+        {
+            T[] temparray = arrays;
+            arrays = new T[arrays.Length + 1];
+            for (int i = 0; i < temparray.Length; i++)
+            {
+                arrays[i] = temparray[i];
+            }
+            arrays[arrays.Length - 1] = array;
+        }
+    }
+}
---
+++ MyDictonary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace MyDictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> meyveler = new MyDictionary<string>();
+            meyveler.Add("ayva");
+            meyveler.Add("armut");
+            meyveler.Add("kayısı");
+            meyveler.Add("şeftali");
+            meyveler.Add("elma");
+
+            my
+
+            Console.WriteLine(meyveler);
+        }
+    }
+
+}
